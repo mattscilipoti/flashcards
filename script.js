32d046035e1510ebfd0a413e7d5c45c78956a741
Justin Price <Justin@Justins-MacBook-Pro.local>
2015-07-13 14:00:59 -0400

    added code to js file that makes the index cards disappear when the prompt is answered

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
   $(".box1").on("click", function() {
     prompt("who are my favorite recording artists?");
-    $(".music").css("display", "block");
+    $(".box1").css("display", "none");
   })
   $(".box2").on("click", function() {
     prompt("what are my favorite movies?");
+    $(".box2").css("display", "none");
   })
   $(".box3").on("click", function() {
     prompt("what's my favorite food?");
+    $(".box3").css("display", "none");
   })
   $(".box4").on("click", function() {
     prompt("what's my favorite color?");
+    $(".box4").css("display", "none");
   })
   $(".box5").on("click", function(){
     prompt("am i a dog person or a cat person?");
+    $(".box5").css("display", "none");
   })
   $(".box6").on("click", function() {
     prompt("what are my least favorite things?");
+    $(".box6").css("display", "none");
   })
 })
